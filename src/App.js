07a773b4905874f436e877c8e60c6c431b777fc0
Ali <ar.k@outlook.com>
2022-01-27 07:53:14 -0800

    create Title and Todo components to show the UI

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import TitleSection from './components/TitleSection';
+import TodoSection from './components/TodoSection';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <TitleSection />
+      <TodoSection />
     </div>
   );
 }
