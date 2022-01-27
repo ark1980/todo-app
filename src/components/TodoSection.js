092171bcd354c503ac96286c1f7ed7479d5cd33c
Ali <ar.k@outlook.com>
2022-01-27 12:56:43 -0800

    add more styling for the Todo section

--- src/components/TodoSection.js
+++ src/components/TodoSection.js
@@ -1,8 +1,20 @@
 const TodoSection =  () => {
+
   return (
     <div className="todos-section">
       <div className="todos-container">
-        
+      <div className="todos-header">
+        <h2>Hello User</h2>
+        <p>What are your plans for the day?</p>
+      </div>
+      <div className="todos-body">
+      <div className="todos-body-title">
+        <h2>Tasks</h2>
+          <span>Today</span>
+          <span>Jan/23</span>
+      </div>
+        <hr />
+      </div>
       </div>
     </div>
   )
